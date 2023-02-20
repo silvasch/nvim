f365a752913272173a1b4c2f25fbbf1b02306bfa
Silvan Schmidt <silvan.genius@gmail.com>
2023-02-20 13:50:14 +0100

    Add comments to presets/init.lua

--- lua/configs/preset/init.lua
+++ lua/configs/preset/init.lua
@@ -6,6 +6,7 @@ return {
     colorscheme = nil,
 
     -- options
+    -- you would set them with `:set k=v`, or in lua `vim.opt[k] = v`
 	opts = {
 		-- four wide tabs
 		shiftwidth = 4,
@@ -16,9 +17,16 @@ return {
 		number = true,
 		relativenumber = true,
 	},
-	g = {},
+
+    -- variables
+    -- you would set them with `:let k=v`, or in lua `vim.g[k] = v`
+	g = {
+        settimeoutlen = 150,
+    },
 
     -- mappings
+    -- first layer is the mode that the mapping should be activated in
+    -- second layer are the keybindings, with the structure of which-key (https://github.com/folke/which-key.nvim#%EF%B8%8F-mappings)
     mappings = {
         n = {
             ["<leader>"] = {
@@ -33,10 +41,28 @@ return {
     },
 
     -- plugins
+    -- this table gets passed directly to lazy.nvim, the plugin manager
+    -- look at the structure here: https://github.com/folke/lazy.nvim#-plugin-spec
     plugins = {},
-    -- disable_autopairs = false,
-    -- disable_indent_hints = false,
+    
+    -- this configuration installs three default plugins: folke/which-key.nvim, windwp/nvim-autopairs
+    -- and lukas-reineke/indent-blankline.nvim
+    -- two of them can be disabled here
+    disable_autopairs = false,
+    disable_indent_hints = false,
 
     -- autocmds
+    -- example:
+    -- autocmds = {
+    --     {
+    --         event = "VimEnter",
+    --         cmd = {
+    --             desc = "Example autocommand",
+    --             callback = function()
+    --                 print("Ran the example autocommand")
+    --             end
+    --         }
+    --     }
+    -- }
     autocmds = {},
 }
