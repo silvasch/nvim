9832301b31e1a7876c0efea1e752f71d8422fe88
Silvan Schmidt <silvan.genius@gmail.com>
2023-02-21 09:44:00 +0100

    Remove dev

--- lua/profiles/minimal/init.lua
+++ lua/profiles/minimal/init.lua
@@ -1,4 +1,6 @@
-local utils = require("profiles.minimal.utils")
+local profile_name = "minimal"
+
+local utils = require("profiles." .. profile_name ..".utils")
 
 return {
     -- global configs
@@ -15,6 +17,8 @@ return {
         -- line numbers
         number = true,
         relativenumber = true,
+
+        timeoutlen = 150,
     },
     -- variables
     -- you would set them with `:let k=v`, or in lua `vim.g[k] = v`
@@ -30,7 +34,11 @@ return {
                 w = { utils.save_file, "Save the current file" },
                 q = { "<cmd>quit<cr>", "Quit the editor" },
                 n = { "<cmd>enew<cr>", "Create a new file" },
-            }
+                f = {
+                    "<cmd>Telescope find_files<cr>",
+                    "Open the file picker"
+                },
+            },
         },
         i = {},
         v = {},
@@ -61,7 +69,50 @@ return {
         },
 
         -- treesitter
-        { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            opts = {
+                ensure_installed = { "c", "lua", "vim", "help", "rust", "python" },
+
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                -- List of parsers to ignore installing (for "all")
+                ignore_install = {},
+
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                parser_install_dir = vim.fn.stdpath("data") .. "/treesitter/" .. profile_name, -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+                highlight = {
+                    enable = true,
+
+                    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+                    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+                    -- the name of the parser)
+                    -- list of language that will be disabled
+                    -- disable = {},
+                    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        },
 
         -- themes
         {
