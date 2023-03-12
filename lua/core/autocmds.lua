124d8a2de955af3d2b700b3df21e44a78b81d868
Silvan Schmidt <sil.schmidt@proton.me>
2023-03-12 16:07:30 +0100

    init

--- lua/core/autocmds.lua
+++
@@ -1,5 +0,0 @@
-return function(cmds)
-    for _, cmd in ipairs(cmds) do
-        vim.api.nvim_create_autocmd(cmd.event, cmd.cmd)
-    end
-end
--- lua/core/mappings.lua
+++
@@ -1,6 +0,0 @@
-return function(mappings)
-    local wk = require("which-key")
-    for mode, mapping in pairs(mappings) do
-        wk.register(mapping, { mode = mode })
-    end
-end
